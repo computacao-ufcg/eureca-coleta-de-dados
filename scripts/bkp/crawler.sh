3e1631180d838796f87ac7758ffa4be3fb9323d3
fubica <fubica@dsc.ufcg.edu.br>
2020-07-16 17:33:59 -0300

    Remocao de scripts antigos

--- scripts/bkp/crawler.sh
+++
@@ -1,2 +0,0 @@
-node ../crawlers/get_page.js --login=$1 --l=$2 --o=$3 --p=$4
-
--- scripts/bkp/extrai-urls-turmas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-periodo=$1
-dir_fonte=$2
-dir_destino=$3
-
-python /Users/fubica/Documents/fubica/ccc/src/report-matricula/src/parsers/turmas/turmas-ofertadas_parse.py $dir_fonte/turmas-$periodo.html | awk -F ";" '{ print "https://pre.ufcg.edu.br:8443/ControleAcademicoOnline/Controlador?command=CoordenacaoTurmaResumo&codigo="$1"&turma="$2 }' > $dir_destino/url-turmas-$periodo.dat
-
